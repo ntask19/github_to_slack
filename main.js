b55a86166611e77029a43ea1eb84f1ed767d6d2d
Task Nagashige <task.19.xx@gmail.com>
2020-03-16 11:17:38 +0900

    GitHubの未読メールを取得して削除するようにした

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 function myFunction() {
-  
+
+}
+
+function getGitHubMail() {
+  var searchCondition = 'is:unread from:notifications@github.com';
+  var threads = GmailApp.search(searchCondition, 0, 50);
+
+  for (var i = 0 ; i < threads.length; i++) {
+    var msgs = GmailApp.getMessagesForThread(threads[i]);
+
+    for (var j = 0; j < msgs.length; j++) {
+      Logger.log(msgs[j].getSubject());
+      Logger.log(msgs[j].getBody());
+    }
+
+    threads[i].moveToTrash();
+  }
 }
